08ab5cb78f6183254d17719234ca11d5bacb371f
Jacob Hinman <78567796+JSHinman@users.noreply.github.com>
2022-10-03 20:42:16 +1300

    Started adding shooting

--- FPS Game/Assets/Scripts/ShootGun.cs
+++ FPS Game/Assets/Scripts/ShootGun.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ShootGun : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform bulletStartPos;
+    public LayerMask layerMask;
+
     void Start()
     {
         
@@ -13,6 +15,21 @@ public class ShootGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1")) {
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(bulletStartPos.position, transform.TransformDirection(Vector3.back), out hit, Mathf.Infinity, layerMask))
+            {
+                if (hit.transform.name == "Cube") {
+                    Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+                    rb.AddForce(transform.TransformDirection(Vector3.back), ForceMode.Impulse);
+                }
+            }
+            else
+            {
+               // Debug.DrawRay(bulletStartPos.position, transform.TransformDirection(Vector3.back) * 10000f, Color.white);
+                //Debug.Log("Did not Hit");
+            }
+        }
     }
 }
